53f9a87585ed7693ba092b96ca2a9534400c620f
JaeyeonKIM <foodsksms@gmail.com>
2020-02-26 12:42:03 +0900

    [issue-3] Basket정의
    - Basket, BasketDto정의

--- src/main/java/com/example/demo/domain/Basket.java
+++ src/main/java/com/example/demo/domain/Basket.java
@@ -1,4 +1,31 @@
 package com.example.demo.domain;
 
+import com.example.demo.dto.BasketDto;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@ToString
+@NoArgsConstructor
 public class Basket {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @OneToMany
+    private List<Good> goods = new ArrayList<>();
+
+    public BasketDto of() {
+        return new BasketDto(this.goods);
+    }
 }
